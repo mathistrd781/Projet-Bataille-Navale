8ed69c084e9a9639dfa3ee80a71ead3459039ff0
Chturm <103414565+Chturm@users.noreply.github.com>
2022-12-03 00:55:47 +0100

    Update Jeu.c

--- Jeu.c
+++ Jeu.c
@@ -1,27 +1,56 @@
-//
-// Created by mathi on 14/11/2022.
-//
-
 #include "header.h"
 
-void jeu(){
 
-    char ** plateauBateau = genererPlateau(HAUTEUR, LARGEUR);
-    char ** plateauTirs = genererPlateau(HAUTEUR, LARGEUR);
-    char ** plateauEnnemi = genererPlateau(HAUTEUR, LARGEUR);
+void jeu(int choixDifficulte, char ** plateau_bateau, char ** plateau_tirs, char ** plateau_ennemi){
 
-    gotoligcol(1, 7);
-    printf("Vos Bateaux :");
-    affichagePlateau(plateauBateau, 3, 2);
-
-    gotoligcol(1, 85);
-    printf("Vos tirs :");
-    affichagePlateau(plateauTirs, 3, 80);
+    vider_Plateau(plateau_bateau);
+    vider_Plateau(plateau_tirs);
+    vider_Plateau(plateau_ennemi);
 
 
+    for (int i = 0; i < NOMBRE_SOUS_MARIN; i++)
+    {
+        placementBateau(plateau_bateau, SOUS_MARIN);
+    }
+    for (int i = 0; i < NOMBRE_DESTROYER; i++)
+    {
+        placementBateau(plateau_bateau, DESTROYER);
+    }
+
+    for(int i =0; i < NOMBRE_CROISEUR ; i++)
+    {
+        placementBateau(plateau_bateau, CROISEUR);
+    }
+
+    placementBateau(plateau_bateau, PORTE_AVION);
 
 
-    freePlateau(plateauBateau, HAUTEUR);
-    freePlateau(plateauTirs, HAUTEUR);
-    freePlateau(plateauEnnemi, HAUTEUR);
+    for (int i = 0; i < NOMBRE_SOUS_MARIN; i++)
+    {
+        placementBateau(plateau_ennemi, SOUS_MARIN);
+    }
+    for (int i = 0; i < NOMBRE_DESTROYER; i++)
+    {
+        placementBateau(plateau_ennemi, DESTROYER);
+    }
+
+    for(int i =0; i < NOMBRE_CROISEUR ; i++)
+    {
+        placementBateau(plateau_ennemi, CROISEUR);
+    }
+
+    placementBateau(plateau_ennemi, PORTE_AVION);
+
+    choixDifficulte = choixIA ();
+
+    system("cls");
+}
+
+void boucle_de_jeu (int choixDifficulte, char ** plateau_bateau, char ** plateau_tirs, char ** plateau_ennemi)
+{
+    affichagePlateauJoueur (plateau_bateau, plateau_tirs, plateau_ennemi);
+
+    actionJoueur(choixDifficulte, plateau_bateau, plateau_tirs, plateau_ennemi);
+
+    IA (choixDifficulte, plateau_bateau, plateau_tirs, plateau_ennemi);
 }
