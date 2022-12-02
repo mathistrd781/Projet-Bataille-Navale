50bcbf0cc4d66a2ab3ed1420dc1abd845c778ef6
Chturm <103414565+Chturm@users.noreply.github.com>
2022-12-03 00:56:07 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,21 @@
 
 int main()
 {
+
+    srand(time(NULL));
+
+    char ** plateau_bateau = genererPlateau(HAUTEUR, LARGEUR);
+    char ** plateau_tirs = genererPlateau(HAUTEUR, LARGEUR);
+    char ** plateau_ennemi = genererPlateau(HAUTEUR, LARGEUR);
+
+
     hide_cursor(0);
     //presentation();
-    menu();
+    menu(plateau_bateau, plateau_tirs, plateau_ennemi);
+
+    freePlateau(plateau_bateau, HAUTEUR);
+    freePlateau(plateau_tirs, HAUTEUR);
+    freePlateau(plateau_ennemi, HAUTEUR);
 
     return 0;
 }
